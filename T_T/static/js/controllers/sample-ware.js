a4ef5357054dd7474ea89c8f7df5bf915cb5463b
Soheil Behnezhad <soheil.behnezhad@gmail.com>
2013-11-15 23:31:14 +0330

    Slight changes to main slider and sample product page

--- T_T/static/js/controllers/sample-ware.js
+++ T_T/static/js/controllers/sample-ware.js
@@ -1,34 +1,36 @@
-T_T.controller('CommentCtrl', function ($scope, $http) {
+T_T.controller('CommentCtrl', ['$scope', '$http', 'shoppingCart',
+    function ($scope, $http, shoppingCart) {
 
-    $scope.list = {
-        "result": 1,
-        "commentList": [
-            {
-                "name": "جبار",
-                "message": "خیلی خوبه!"
-            }
-        ]
-    };
+        $scope.list = {
+            "result": 1,
+            "commentList": [
+                {
+                    "name": "جبار",
+                    "message": "خیلی خوبه!"
+                }
+            ]
+        };
 
-    $scope.addComment = function() {
-        $scope.list.commentList.push({name:$scope.name, message:$scope.comment});
-        $scope.name="";
-        $scope.comment="";
-    };
+        $scope.addComment = function () {
+            $scope.list.commentList.push({name: $scope.name, message: $scope.comment});
+            $scope.name = "";
+            $scope.comment = "";
+        };
 
-    $scope.addToCart = function (id) {
-        shoppingCart.addToCart(id);
-    };
+        $scope.addToCart = function (id) {
+            shoppingCart.addToCart(id);
+        };
 
 
-    var id = WaitMsg.add("در حال دریافت کامنت ها");
-    $http({method: 'POST', url: 'http://webproject.roohy.me/ajax/2/90109903/comment/list'}).
-        success(function(data, status, headers, config) {
-            $scope.list = data;
-            WaitMsg.success(id);
-        }).
-        error(function(data, status, headers, config){
-            console.log("ERROR", data);
-            WaitMsg.error(id);
-        });
-});
+        var id = WaitMsg.add("در حال دریافت کامنت ها");
+        $http({method: 'POST', url: 'http://webproject.roohy.me/ajax/2/90109903/comment/list'}).
+            success(function (data, status, headers, config) {
+                $scope.list = data;
+                WaitMsg.success(id);
+            }).
+            error(function (data, status, headers, config) {
+                console.log("ERROR", data);
+                WaitMsg.error(id);
+            });
+    }
+]);
