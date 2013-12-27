9bcb6d5d08f22020c87f3479a9179b7643e51023
Soheil Behnezhad <soheil.behnezhad@gmail.com>
2013-12-27 17:44:42 +0330

    Implement home page view function

--- T_T/urls.py
+++ T_T/urls.py
@@ -3,6 +3,7 @@ from django.conf.urls import patterns, include, url
 from django.contrib import admin
 from django.views.generic.base import TemplateView
 from T_T import settings
+from shop.views import home
 
 admin.autodiscover()
 
@@ -12,7 +13,7 @@ urlpatterns = patterns('',
     # url(r'^blog/', include('blog.urls')),
 
     url(r'^admin/', include(admin.site.urls)),
-    url(r'^home/$', TemplateView.as_view(template_name='home.html')),
+    url(r'^$', home),
     url(r'^list/$', TemplateView.as_view(template_name='list.html')),
     url(r'^products/sample/$', TemplateView.as_view(template_name='sample-product.html')),
     url(r'^moderate/add-product/$', TemplateView.as_view(template_name='add-product.html')),
--- shop/views.py
+++ shop/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from shop.models import SliderItem
 
-# Create your views here.
+
+def home(request):
+    slider_items = SliderItem.objects.all()
+    return render(request, 'home.html', {
+        'slider_items': slider_items,
+    })
+
